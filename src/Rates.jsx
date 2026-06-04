761378d5a4d9ac57c35f89da67773bcc7fa6d2e8
mayamartinezix <mayamartinez.ix@gmail.com>
2026-06-03 19:44:40 -0600

    Update Rates.jsx

--- src/Rates.jsx
+++ src/Rates.jsx
@@ -21,7 +21,7 @@ const RATES_FALLBACK = [
     name: 'Bike rack',
     price: '+ $20',
     unit: 'adventures everywhere you go',
-    desc: 'Want a great rack? We got you.', 
+    desc: 'Need a great rack? We can handle that for you.', 
     
   },
 ];
