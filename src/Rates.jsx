8e8d84cd1c992b0803a79514982992d3698295ab
mayamartinezix <mayamartinez.ix@gmail.com>
2026-06-05 15:57:58 -0600

    Update Rates.jsx

--- src/Rates.jsx
+++ src/Rates.jsx
@@ -32,7 +32,7 @@ const RATES = [
     desc: 'We go the extra mile.',
   },
   {
-    icon: 'phone',
+    icon: 'CutePhone',
     name: 'Custom Shuttle Service',
     price: 'Call us for a quote',
     unit: 'Anywhere you want',
