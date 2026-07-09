f0457131c2f41033a6c186090eb3458f4e14401d
mayamartinezix <mayamartinez.ix@gmail.com>
2026-07-08 17:41:40 -0700

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -18,10 +18,7 @@ function Hero({ onReserve }) {
           <Button variant="primary" onClick={onReserve}>Saddle up</Button>
           <Button variant="secondary" onClick={() => { window.location.hash = 'trail'; }}>The Trail</Button>
         </div>
-        <a className="sb-hero__link" href="tel:+12085550148">
-          <i data-lucide="phone"></i> Or give us a call
-        </a>
-      </div>
+       
 
       <div className="sb-hero__photo">
         <span className="sb-hero__badge">Hop on!</span>
